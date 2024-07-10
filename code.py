adabc71042b4521cbf6a513780de52222a1e6ddb
Anvay <130112269+anvayajmera@users.noreply.github.com>
2024-07-10 16:54:56 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,25 @@
-from ev3dev2.motor import LargeMotor, OUTPUT_A
-from ev3dev2.motor import SpeedPercent
+import time
+import ev3dev.ev3 as ev3
 
-# Initialize the motor connected to port A
-motor = LargeMotor(OUTPUT_A)
+motor = ev3.LargeMotor('outA')
 
-# Set the motor to run at 100% speed
-motor.on(SpeedPercent(100))
+def set_motor_speed(speed):
+    motor.run_forever(speed_sp=speed)
+
+def stop_motor():
+    motor.stop(stop_action="brake")
+
+def water_jet(duration, speed):
+    set_motor_speed(speed)
+    time.sleep(duration)
+    stop_motor()
+
+def main():
+    jet_duration = 5
+    jet_speed = 900
+    for _ in range(3):
+        water_jet(jet_duration, jet_speed)
+        time.sleep(2)
+
+if __name__ == "__main__":
+    main()
